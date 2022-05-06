ea549f1087781f3c6fccd9c54062e36baeb75dd5
niranjanv849 <niranjanv849@gmail.com>
2022-05-06 15:46:56 +0530

    Today wrap up 🥱 | 🛠

--- src/TaskOne.js
+++ src/TaskOne.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./TaskOne.css";
 import { ReplayOutlined } from "@material-ui/icons";
-import grey from "@material-ui/core/colors/grey";
-import { useState, useEffect } from "react";
+// import grey from "@material-ui/core/colors/grey";
+import { useState } from "react";
 
 function TaskOne() {
-  const [Temp, setTemp] = useState();
+  // const [Temp, setTemp] = useState();
+  const celsius = "";
+  const fahrenheit = "";
+
+  function toCelsius(fahrenheit) {
+    return ((fahrenheit - 32) * 5) / 9;
+  }
+
+  // function toFahrenheit(celsius) {
+  //   return (celsius * 9) / 5 + 32;
+  // }
 
   return (
     <div className="taskOne">
@@ -18,12 +28,16 @@ function TaskOne() {
           className="conversion__input"
           type="number"
           placeholder="1 Celsius"
+          // value={celsius}
+          // onChange={this.handleCelsiusChange}
         />
         <ReplayOutlined style={{ color: "grey" }} />
         <input
           className="conversion__input"
           type="number"
           placeholder="33.8 Fahrenheit"
+          // value={fahrenheit}
+          // onChange={this.handleFahrenheitChange}
         />
       </div>
     </div>
