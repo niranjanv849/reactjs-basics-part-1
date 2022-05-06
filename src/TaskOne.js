1e2e9782f1ea90cc3282b08bc31b8f3d6378e8eb
niranjanv849 <niranjanv849@gmail.com>
2022-05-06 14:41:25 +0530

    Task -1 🎨 Ui Done | 🛠 Functionalities started 🚀

--- src/TaskOne.js
+++ src/TaskOne.js
@@ -1,8 +1,33 @@
 import React from "react";
 import "./TaskOne.css";
+import { ReplayOutlined } from "@material-ui/icons";
+import grey from "@material-ui/core/colors/grey";
+import { useState, useEffect } from "react";
 
 function TaskOne() {
-  return <div className="taskOne">TaskOne</div>;
+  const [Temp, setTemp] = useState();
+
+  return (
+    <div className="taskOne">
+      <div>
+        <span className="conve">Conv:</span> Celsius 🔄 Fahrenheit
+      </div>
+      <div>Simple, quick °C to °F conversion</div>
+      <div className="conversion__container">
+        <input
+          className="conversion__input"
+          type="number"
+          placeholder="1 Celsius"
+        />
+        <ReplayOutlined style={{ color: "grey" }} />
+        <input
+          className="conversion__input"
+          type="number"
+          placeholder="33.8 Fahrenheit"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default TaskOne;
